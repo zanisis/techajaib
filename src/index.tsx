73ea42648950db2c8598ac33a2137998acb65d66
zanisis <adzannyaa@gmail.com>
2022-04-26 19:42:35 +0700

    commit comment

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
+// testcomment
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
